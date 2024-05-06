edebfdde20ad0724726222076aac52ac819383c8
GoncaloaaF <goncaloaaf@gmail.com>
2024-05-06 16:19:24 +0100

    Estrutura Sequencial v1

---
+++ 0789 - Fundamentos de linguagem JAVA/EstruturaSequencial/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ex1 1  -\n");
+
+        System.out.println("Hello world!");
+
+
+        System.out.println("\nEx1 2  -\n");
+        /*
+
+        Faça um Programa que peça um número e então mostre a
+         mensagem O número informado foi [número].
+
+         */
+
+        int num = 10;
+
+        System.out.println("O número informado foi " + num);
+
+    }
+}
