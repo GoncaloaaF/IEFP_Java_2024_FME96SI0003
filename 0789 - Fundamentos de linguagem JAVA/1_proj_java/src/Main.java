45d84e889052944ac37e7506995f1b3b5a5620c6
GoncaloaaF <goncaloaaf@gmail.com>
2024-05-06 14:09:41 +0100

    escrever para a consola

---
+++ 0789 - Fundamentos de linguagem JAVA/1_proj_java/src/Main.java
@@ -0,0 +1,32 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Comentario numa linha
+
+        /*
+            Comentario
+            em
+            mais
+            de
+            uma
+            linha
+
+         */
+         // System.out.println("Hello world!"); // escreve para a consola
+        System.out.println("Ola Mundo");
+        System.out.println("Bem Vindo ao JAVA");
+        System.out.println("Ola Mundo");
+        System.out.println(10);
+        System.out.println( 10 * 3);
+        System.out.println( 10 + 3);
+        System.out.println( 10 - 3);
+        System.out.println( 10 / 3);
+        System.out.println("Ola Mundo " + 2024);
+        System.out.println("Ola Mundo " + "Texto 2");
+
+
+
+    }
+}
