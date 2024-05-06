3dd6e960d0c74c22d6a6a88cc7e86f16327037f9
GoncaloaaF <goncaloaaf@gmail.com>
2024-05-06 16:08:58 +0100

    var v3

---
+++ 0789 - Fundamentos de linguagem JAVA/var_v2/src/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int val1 = 10;
+        int val2 = 20;
+        int soma = val1 + val2;
+        System.out.println("a soma é: " + soma);
+
+
+        val1 = 50;
+        val2 = 31;
+        soma = val1 + val2;
+        System.out.println("a soma 2 é: " + soma);
+
+
+
+        int v3 = 10;
+        float f1 = 10.3f;
+        double d1 = 10.3d;
+
+        double soma_float = v3 + f1 + d1;
+
+        System.out.println("soma_float: " + soma_float);
+
+
+
+        float soma2 = f1 * f1;
+        System.out.println("soma2: " + soma2);
+
+
+
+        double somad = d1 * d1;
+        System.out.println("somad: " + somad);
+
+
+
+
+
+        double soma22 = f1 * f1;
+        System.out.println("soma2: " + soma22);
+
+
+
+        // float somad2 = d1 * d1;
+        // System.out.println("somad: " + somad);
+
+
+        //Float teste  = 12f;
+        float teste6  = 10; // 10.0000000
+        double foo = 123; // 10.00000000000000
+
+        //  System.out.println(teste);
+        System.out.println(teste6);
+        System.out.println(foo);
+
+
+        int v10 = 10;
+        int v20 = 20;
+        soma = v10 + v20;
+        System.out.println(soma); // 30
+
+        v10 = v10 * (2 + v20);
+        System.out.println(v10);
+
+
+
+        System.out.println(v10 + "" + v20);
+
+    }
+}
