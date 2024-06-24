aa414316b9e67f660d7b5f485f8080fbb8621f9c
GoncaloF <goncaloaaf@gmail.com>
2024-06-24 15:37:57 +0100

    herança

---
+++ 0789 - Fundamentos de linguagem JAVA/intrefaces/intrefaces/src/Veleiro.java
@@ -0,0 +1,17 @@
+public class Veleiro implements Veiculo {
+
+    @Override
+    public String tipo() {
+        return "Barco";
+    }
+
+    @Override
+    public String marca() {
+        return "Beneteau";
+    }
+
+    @Override
+    public String modelo() {
+        return "Flyer 8 SUNdeck";
+    }
+}
